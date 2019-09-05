ba5cb554f118ebb836b5dfc1b250e9b55ed79cd1
qiaokun <qkbeyond@gmail.com>
2019-09-05 15:18:48 +0800

    页面title修改为动态的，会随着页面的变化而变化，不再是写死的

--- src/permission.js
+++ src/permission.js
@@ -18,6 +18,9 @@ const whiteList = ['/login', '/auth-redirect']// no redirect whitelist
 
 router.beforeEach((to, from, next) => {
   NProgress.start() // start progress bar
+  if (to.meta.title) {
+    document.title = to.meta.title + ' - Vue Element Admin'
+  }
   if (getToken()) { // determine if there has token
     /* has token*/
     if (to.path === '/login') {
